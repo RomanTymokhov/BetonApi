5249234a5cb8abe3064e448154456f57063aa9ec
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2018-10-19 20:18:52 +0300

    Работоспособный клиент

---
+++ BetonApi/BetonApi/BetonClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using BetonApi.Extentions;
+
+namespace BetonApi
+{
+    public class BetonClient
+    {
+        private readonly string baseUrl = "https://www.betonsuccess.ru";
+        private readonly string urlSegment = "/capper/api/csv_post/";
+        private readonly Dictionary<string, string> reqParams;
+
+        private readonly HttpClient httpClient;
+
+
+        public BetonClient(short sid, string key)
+        {
+            httpClient = new HttpClient { BaseAddress = new Uri(baseUrl) };
+
+            reqParams = new Dictionary<string, string>
+            {
+                ["sid"] = sid.ToString(),
+                ["key"] = key
+            };
+        }
+
+        public async Task<string> SendPick(string content, byte email = 0, byte publish = 0)
+        {
+            reqParams.Add("email", email.ToString());
+            reqParams.Add("publish", publish.ToString());
+
+            var response = await httpClient.PostAsync(urlSegment.Build(reqParams),
+                new StringContent(content, Encoding.ASCII, "application/x-www-form-urlencoded")).ConfigureAwait(false);
+
+           return await Task.Factory.StartNew(() => response.Content.ReadAsStringAsync().Result);
+        }
+    }
+}
---
+++ BetonApi/BetonApi/Extentions/StringExtetion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace BetonApi.Extentions
+{
+    public static class StringExtetion
+    {
+        public static string Build(this  string urlSeg, Dictionary<string, string> dict)
+        {
+            var url = new StringBuilder(urlSeg);
+            url.AppendFormat("?{0}", BuildRequestData(dict));
+
+            return url.ToString();
+        }
+
+        internal static string BuildRequestData(IDictionary<string, string> dict, bool escape = true) => string.Join("&", dict.Select(kvp =>
+                 string.Format("{0}={1}", kvp.Key, escape ? HttpUtility.UrlEncode(kvp.Value) : kvp.Value)));
+    }
+}
